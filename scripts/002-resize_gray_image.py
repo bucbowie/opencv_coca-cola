6f01776062e097e6222fc77460c23b8f5964bd7c
jaskew <bucbowie@gmail.com>
2019-05-07 08:07:13 -0400

    fix 002 syntx err

--- scripts/002-resize_gray_image.py
+++ scripts/002-resize_gray_image.py
@@ -2,6 +2,7 @@ import urllib
 import cv2
 import numpy as np
 import os
+import errno
 
 '''002-if-000-fails-resize.py reads in 
 a list of images and does 2 things:
@@ -12,14 +13,14 @@ a list of images and does 2 things:
 def check_directories(in_dir):
 #--------------------------------#
     if not os.path.exists(in_dir):
-        print("Error: 002-if-000-fails-resize.py \n--------------------------------#\nThe needed input directory: {0}\ndoes not exist and impedes running this program.\nEnsure directory {0} exists, is readable,\n".format(in_dir)
-        raise FileNotFoundError
+        print("Error: 002-if-000-fails-resize.py \n--------------------------------#\nThe needed input directory: {0}\ndoes not exist and impedes running this program.\nEnsure directory {0} exists, is readable,\n".format(in_dir))
+        raise ValueError("Unable to find or access needed input ==>",  in_dir)
 
 #--------------------------------#
 def ensure_directories(my_dir):
 #--------------------------------#
     if not os.path.exists(my_dir):
-        os.path.makedirs(my_dir)
+        os.makedirs(my_dir)
 
 #--------------------------------#
 def scrap_raw():
@@ -31,7 +32,7 @@ def scrap_raw():
             if os.path.getsize(scrape_dir + "/" + jpg) > size_smallest_img:
                 img = cv2.imread(scrape_dir + "/"  + jpg, cv2.IMREAD_GRAYSCALE)
                 resized_image = cv2.resize(img, (100, 100))
-                cv2.imwrite(in_neg_dir + "/" + jpg, resized_image)
+                cv2.imwrite(in_dir + "/" + jpg, resized_image)
             else:
                 print("WARNING: Skipping image",scrape_dir + "/"  + jpg, "of size", os.path.getsize(scrape_dir + "/" + jpg))
         except Exception as e:
@@ -45,12 +46,12 @@ def scrap_raw():
 #
 size_smallest_img = (1024 * 5)   #Any image smaller considered blank or corrupt, we will skip these.
 scrape_dir =  '/mnt/usb/raw_neg' #The place to store the freshly scraped images.
-in_neg_dir =  '/mnt/usb/in_neg'  #Formatted negative images which will feed 'out_neg.' 
+in_dir =  '/mnt/usb/in_neg'  #Formatted negative images which will feed 'out_neg.' 
 #
 #Ensure inputs and outputs exists
-check_directories(in_dir)        #Ensure inputs exist before running main logix.
-ensure_directories(in_neg_dir)   #Ensure the output directory is defined to hold outputs.
+check_directories(scrape_dir)        #Ensure inputs exist before running main logix.
+ensure_directories(in_dir)   #Ensure the output directory is defined to hold outputs.
 #
 #Process images: reading from input; write to output.
 #
-scrap_raw(scrape_dir)            #Read each image, grayscale and resize - write to outdir.
+scrap_raw()            #Read each image, grayscale and resize - write to outdir.
